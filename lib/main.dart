4625daeecaebd1385b62b4d3a7dbc39088a964e6
gustavo <gsarruda.office@gmail.com>
2022-06-22 20:12:32 -0300

    changed the -MyApp- widget from stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print("Answer Chosen!");
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
   }
 
   @override
@@ -24,18 +37,20 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text("The question!"),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text("Answer 1"),
               onPressed: answerQuestion,
             ),
             RaisedButton(
-              child: Text("Answer 2"),
-              onPressed: () => print("Answer 2 chosen!"),
-            ),
+                child: Text("Answer 2"),
+                onPressed: () =>
+                    print("Answer 2 chosen!") // Anonymous arrow function
+                ),
             RaisedButton(
               child: Text("Answer 3"),
               onPressed: () {
+                // Anonymous function
                 print("Answer 3 chosen!");
               },
             )
